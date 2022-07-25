3ba123811536c7fa26418cccf59b66271a4e4d3e
eduardo <cezamperlini@gmail.com>
2022-07-25 19:04:19 -0300

    Data automatica

--- core/models.py
+++ core/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from datetime import datetime
+
 
 # Create your models here.
 
@@ -34,6 +36,9 @@ class Patrimonio(models.Model):
     )
 
     dt_cadastro = models.DateField(
+        default=datetime.now()
+        
+        
         
     )
 
